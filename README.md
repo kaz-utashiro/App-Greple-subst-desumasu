53da14dfbc543e02301f4634d8914440161c395d
Kazumasa Utashiro <kaz@utashiro.com>
2021-05-28 10:07:22 +0900

    minil new App::Greple::subst::desumasu

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Greple::subst::desumasu - It's new $module
+
+# SYNOPSIS
+
+    use App::Greple::subst::desumasu;
+
+# DESCRIPTION
+
+App::Greple::subst::desumasu is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/Greple/subst/desumasu.pm
@@ -0,0 +1,39 @@
+package App::Greple::subst::desumasu;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::subst::desumasu - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::subst::desumasu;
+
+=head1 DESCRIPTION
+
+App::Greple::subst::desumasu is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
