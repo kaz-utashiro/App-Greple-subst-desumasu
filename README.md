d4985bae7be09a1021adf76d13b4825d52decdc2
Kazumasa Utashiro <kaz@utashiro.com>
2021-05-30 23:54:41 +0900

    Document update.

--- README.md
+++ README.md
@@ -4,43 +4,80 @@ App::Greple::subst::desumasu - Japanese DESU/MASU dictionary for App::Greple::su
 
 # SYNOPSIS
 
-    greple -Msubst::desumasu
+    greple -Msubst::desumasu --dearu --subst --all file
+
+    greple -Msubst::desumasu --dearu --diff file
+
+    greple -Msubst::desumasu --dearu --replace file
 
 # DESCRIPTION
 
-greple -Msubst module based on [desumasu-converter](https://metacpan.org/pod/desumasu-converter).
+greple -Msubst module based on
+[desumasu-converter](https://github.com/kssfilo/desumasu-converter).
 
 This is a simple checker/converter module for Japanese writing style
 so called DUSU/MASU (ですます調: 敬体) and DEARU (である調: 常態).
-This is not my own idea and the dictionary is based on:
-
-    L<https://github.com/kssfilo/desumasu-converter>
+This is not my own idea and the dictionary is based on
+[https://github.com/kssfilo/desumasu-converter](https://github.com/kssfilo/desumasu-converter).
 
 See article [https://kanasys.com/tech/722](https://kanasys.com/tech/722) for detail.
 
 # OPTIONS
 
-- **--desumasu**
-- **--desumasu-n**
-- **--desumasu-N**
-
-    Convert DESU/MASU to DEARU style.
-
-    です and ます sometimes followed by ね (NE), and that ね is removed
-    from converted result by default.  Option with **-n** keep that ね, and
-    option with **-N** igonore them.
-
 - **--dearu**
 - **--dearu-n**
 - **--dearu-N**
 
+    Convert DESU/MASU to DEARU style.
+
+    DESU (です) and MASU (ます) sometimes followed by NE (ね) in frank
+    situation, and that NE (ね) is removed from converted result by
+    default.  Option with **-n** keep that NE (ね), and option with **-N**
+    igonore them.
+
+- **--desumasu**
+- **--desumasu-n**
+- **--desumasu-N**
+
     Convert DEARU to DESU/MASU style.
+
+Use them with **greple** **-Msubst** options.
+
+- **--subst --all --no-color**
+
+    Print converted text.
+
+- **--diff**
+
+    Produce diff output of original and converted text.  Use **cdif**
+    command in [App::sdif](https://metacpan.org/pod/App::sdif) to visualize the difference.
+
+- **--create**
+- **--replace**
+- **--overwrite**
+
+    To update the file, use these options.  Option **--create** make new
+    file with `.new` suffix.  Option **--replace** update the target file
+    with backup, while option **--overwrite** does without backup.
 
 See [App::Greple::subst](https://metacpan.org/pod/App::Greple::subst) for other options.
 
+# INSTALL
+
+## CPANMINUS
+
+From GIT repository:
+
+    cpanm https://github.com/kaz-utashiro/greple-subst-desumasu.git
+
 # SEE ALSO
 
-[App::Greple::subst](https://metacpan.org/pod/App::Greple::subst)
+[App::Greple](https://metacpan.org/pod/App::Greple), [App::Greple::subst](https://metacpan.org/pod/App::Greple::subst)
+
+[App::sdif](https://metacpan.org/pod/App::sdif)
+
+[https://github.com/kssfilo/desumasu-converter](https://github.com/kssfilo/desumasu-converter),
+[https://kanasys.com/tech/722](https://kanasys.com/tech/722)
 
 # AUTHOR
 
--- lib/App/Greple/subst/desumasu.pm
+++ lib/App/Greple/subst/desumasu.pm
@@ -9,7 +9,7 @@ use utf8;
 use App::Greple::subst;
 
 use File::Share qw(:all);
-$ENV{LOCAL_DICT} //= dist_dir __PACKAGE__ =~ s/::/-/gr;
+$ENV{DESUMASU_DICT} = dist_dir __PACKAGE__ =~ s/::/-/gr;
 
 1;
 
@@ -21,17 +21,21 @@ App::Greple::subst::desumasu - Japanese DESU/MASU dictionary for App::Greple::su
 
 =head1 SYNOPSIS
 
-    greple -Msubst::desumasu
+    greple -Msubst::desumasu --dearu --subst --all file
+
+    greple -Msubst::desumasu --dearu --diff file
+
+    greple -Msubst::desumasu --dearu --replace file
 
 =head1 DESCRIPTION
 
-greple -Msubst module based on L<desumasu-converter>.
+greple -Msubst module based on
+L<desumasu-converter|https://github.com/kssfilo/desumasu-converter>.
 
 This is a simple checker/converter module for Japanese writing style
 so called DUSU/MASU (ですます調: 敬体) and DEARU (である調: 常態).
-This is not my own idea and the dictionary is based on:
-
-    L<https://github.com/kssfilo/desumasu-converter>
+This is not my own idea and the dictionary is based on
+L<https://github.com/kssfilo/desumasu-converter>.
 
 See article L<https://kanasys.com/tech/722> for detail.
 
@@ -39,33 +43,72 @@ See article L<https://kanasys.com/tech/722> for detail.
 
 =over 7
 
-=item B<--desumasu>
-
-=item B<--desumasu-n>
-
-=item B<--desumasu-N>
-
-Convert DESU/MASU to DEARU style.
-
-です and ます sometimes followed by ね (NE), and that ね is removed
-from converted result by default.  Option with B<-n> keep that ね, and
-option with B<-N> igonore them.
-
 =item B<--dearu>
 
 =item B<--dearu-n>
 
 =item B<--dearu-N>
 
+Convert DESU/MASU to DEARU style.
+
+DESU (です) and MASU (ます) sometimes followed by NE (ね) in frank
+situation, and that NE (ね) is removed from converted result by
+default.  Option with B<-n> keep that NE (ね), and option with B<-N>
+igonore them.
+
+=item B<--desumasu>
+
+=item B<--desumasu-n>
+
+=item B<--desumasu-N>
+
 Convert DEARU to DESU/MASU style.
+
+=back
+
+Use them with B<greple> B<-Msubst> options.
+
+=over 7
+
+=item B<--subst --all --no-color>
+
+Print converted text.
+
+=item B<--diff>
+
+Produce diff output of original and converted text.  Use B<cdif>
+command in L<App::sdif> to visualize the difference.
+
+=item B<--create>
+
+=item B<--replace>
+
+=item B<--overwrite>
+
+To update the file, use these options.  Option B<--create> make new
+file with C<.new> suffix.  Option B<--replace> update the target file
+with backup, while option B<--overwrite> does without backup.
 
 =back
 
 See L<App::Greple::subst> for other options.
 
+=head1 INSTALL
+
+=head2 CPANMINUS
+
+From GIT repository:
+
+    cpanm https://github.com/kaz-utashiro/greple-subst-desumasu.git
+
 =head1 SEE ALSO
 
-L<App::Greple::subst>
+L<App::Greple>, L<App::Greple::subst>
+
+L<App::sdif>
+
+L<https://github.com/kssfilo/desumasu-converter>,
+L<https://kanasys.com/tech/722>
 
 =head1 AUTHOR
 
@@ -84,13 +127,15 @@ __DATA__
 
 option default -Msubst
 
-option --mydict --dict $ENV{LOCAL_DICT}/$<shift>
+define DICTDIR $ENV{DESUMASU_DICT}
 
-option --desumasu --mydict desumasu.dict
-option --dearu    --mydict dearu.dict
+option --desumasu --dict DICTDIR/desumasu.dict
+option --dearu    --dict DICTDIR/dearu.dict
 
-option --desumasu-n --mydict desumasu-keep-ne.dict
-option --dearu-n    --mydict dearu-keep-ne.dict
+option --desumasu-n --dict DICTDIR/desumasu-keep-ne.dict
+option --dearu-n    --dict DICTDIR/dearu-keep-ne.dict
 
-option --desumasu-N --mydict desumasu-ignore-ne.dict
-option --dearu-N    --mydict dearu-ignore-ne.dict
+option --desumasu-N --dict DICTDIR/desumasu-ignore-ne.dict
+option --dearu-N    --dict DICTDIR/dearu-ignore-ne.dict
+
+#  LocalWords:  diff cdif
